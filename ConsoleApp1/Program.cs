f8066f53298f46953a1c42cd52478f769a1ca935
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2022-05-09 15:06:09 -0300

    Configurações e arquivos diversos de Biblioteca.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using SOLID._3_LSP.Solucao;
+using System;
+
+namespace SOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Violação Lsp 
+            Fatura faturaParcelada = new ParcelarEmSeisVezes();
+            faturaParcelada.FaturaValor = 500.00m;
+            Console.WriteLine(faturaParcelada.ParcelarFatura(faturaParcelada.FaturaValor)); 
+        }
+    }
+}
